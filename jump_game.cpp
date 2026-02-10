c10e3d715539f2cc92005a7abda68e3fc4744736
Rudra-Sangewar <rudrasangewar5@gmail.com>
2026-02-10 21:20:32 +0530

    jump game solution

---
+++ jump_game.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int near = 0, far = 0, jumps = 0;
+
+        while (far < nums.size() - 1) {
+            int farthest = 0;
+
+            for (int i = near; i <= far; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example input
+    vector<int> nums = {2, 3, 1, 1, 4};
+
+    cout << sol.jump(nums) << endl; // Output: 2
+
+    return 0;
+}
