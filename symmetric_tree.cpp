8b52326f03d5a1e106128cfcc84333038ccdc4cf
Rudra-Sangewar <rudrasangewar5@gmail.com>
2026-03-07 13:27:50 +0530

    solution for symettric tree

---
+++ symmetric_tree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) return true;
+        return isMirror(root->left, root->right);
+    }
+
+private:
+    bool isMirror(TreeNode* n1, TreeNode* n2) {
+        if (n1 == nullptr && n2 == nullptr)
+            return true;
+
+        if (n1 == nullptr || n2 == nullptr)
+            return false;
+
+        return (n1->val == n2->val) &&
+               isMirror(n1->left, n2->right) &&
+               isMirror(n1->right, n2->left);
+    }
+};
+
+int main() {
+    /*
+            1
+           / \
+          2   2
+         / \ / \
+        3  4 4  3
+    */
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2, new TreeNode(3), new TreeNode(4));
+    root->right = new TreeNode(2, new TreeNode(4), new TreeNode(3));
+
+    Solution sol;
+    
+    if (sol.isSymmetric(root))
+        cout << "Tree is symmetric\n";
+    else
+        cout << "Tree is not symmetric\n";
+
+    return 0;
+}
