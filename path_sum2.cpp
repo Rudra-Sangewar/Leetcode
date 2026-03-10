d765a421f255d26cf1f54c299624ca592fe73113
Rudra-Sangewar <rudrasangewar5@gmail.com>
2026-03-10 13:38:20 +0530

    solution for path sum 2

---
+++ path_sum2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return generate_trees(1, n);
+    }
+
+private:
+    vector<TreeNode*> generate_trees(int start, int end) {
+
+        if (start > end)
+            return {nullptr};
+
+        vector<TreeNode*> all_trees;
+
+        for (int i = start; i <= end; i++) {
+
+            vector<TreeNode*> left_trees = generate_trees(start, i - 1);
+            vector<TreeNode*> right_trees = generate_trees(i + 1, end);
+
+            for (TreeNode* l : left_trees) {
+                for (TreeNode* r : right_trees) {
+
+                    TreeNode* current_tree = new TreeNode(i);
+
+                    current_tree->left = l;
+                    current_tree->right = r;
+
+                    all_trees.push_back(current_tree);
+                }
+            }
+        }
+
+        return all_trees;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    int n = 3;
+
+    vector<TreeNode*> trees = obj.generateTrees(n);
+
+    cout << "Total Unique BSTs: " << trees.size() << endl;
+
+    return 0;
+}
