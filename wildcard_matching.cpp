61fb7fe271e2d694f9dad98b61e6ac6622532e57
Rudra-Sangewar <rudrasangewar5@gmail.com>
2026-03-19 22:55:46 +0530

    solution for wildcard solution

---
+++ wildcard_matching.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int si = 0, pi = 0, match = 0, star = -1;
+        int sn = s.length(), pn = p.length();
+
+        while (si < sn) {
+            if (pi < pn && (p[pi] == '?' || p[pi] == s[si])) {
+                si++;
+                pi++;
+            } 
+            else if (pi < pn && p[pi] == '*') {
+                star = pi;
+                match = si;
+                pi++;
+            } 
+            else if (star != -1) {
+                pi = star + 1;
+                match++;
+                si = match;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        while (pi < pn && p[pi] == '*') {
+            pi++;
+        }
+
+        return pi == pn;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s = "adceb";
+    string p = "*a*b";
+
+    if (sol.isMatch(s, p)) {
+        cout << "Match\n";
+    } else {
+        cout << "No Match\n";
+    }
+
+    return 0;
+}
