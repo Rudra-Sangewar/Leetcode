b5e61907ba6eddd3af1ce6bfd50fa37c31a62873
Rudra-Sangewar <rudrasangewar5@gmail.com>
2026-04-27 15:20:12 +0530

    solution for rotate array

---
+++ rotate_array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == 0) return;
+
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+
+    obj.rotate(nums, k);
+
+    for(int x : nums)
+        cout << x << " ";
+
+    return 0;
+}
